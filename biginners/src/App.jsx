bdee9a6003dae241c4cc5f15134be7150d4bdaeb
Deodate <ajadeo04@gmail.com>
2024-08-05 22:39:19 +0200

    Progress

--- biginners/src/App.jsx
+++ biginners/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header.jsx"
 import Footer from "./Foot.jsx";
+import Food from "./Food.jsx";
 function App() {
   return(
     <>
     <Header/>
+    <Food/>
     <Footer/>
     </>
   );
---
+++ biginners/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food(){
+
+    const food1 = "Avocado";
+    const food2 = "Banana";
+    const food3 = "Fanta";
+    return(
+        <ul>
+            <li>Apple</li>
+        </ul>
+    );
+}
+
+export default Food
